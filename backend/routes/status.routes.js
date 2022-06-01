5e0e8cfc73307a43877058e7814659a7f0d874d5
Augs Machado <augustomachadoramos@gmail.com>
2022-06-01 08:49:15 -0300

    .

--- backend/routes/status.routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import StatusCtrl from "../controllers/status.controller.js";
-
-const router = express.Router();
-
-router.route("/api").get(StatusCtrl.getStatusApi);
-
-export default router;
